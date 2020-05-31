a0429e5800e4a07be46b08866c79a099ecf017cb
eunbeom.kang <eunbeom.kang@linecorp.com>
2020-06-01 01:03:11 +0900

    차트 제거, URL 수정

--- app.py
+++ app.py
@@ -6,13 +6,11 @@ from datetime import datetime
 import redis as redis
 import requests
 from flask import Flask, request, json, render_template
-from flask_sslify import SSLify
 from linebot import LineBotApi, WebhookHandler
 from linebot.models import MessageEvent, TextMessage, FlexSendMessage, TextSendMessage, \
     BubbleContainer, BoxComponent, TextComponent, FillerComponent, ImageComponent
 
 app = Flask(__name__)
-sslify = SSLify(app)
 
 line_bot_api = LineBotApi(os.environ['LINE_CHANNEL_ACCESS_TOKEN'])
 handler = WebhookHandler(os.environ['LINE_CHANNEL_SECRET'])
@@ -22,11 +20,8 @@ r = redis.from_url(os.environ.get("REDIS_URL"), charset="utf-8", decode_response
 saved_year, saved_month, saved_holiday = 0, 0, []
 
 
-@app.route("/", methods=['GET'])
-def index():
-    if request.method == 'GET':
-        gid = request.args.get('gid')
-
+@app.route("/<gid>", methods=['GET'])
+def index(gid):
     keys = []
     now = datetime.now()
     month = f'{now.year}-{now.month:02d}'
@@ -42,15 +37,11 @@ def index():
 
     length = len(values[1])
     table = [[''] + [str(i + 1) for i in range(length)]]
-    count = [0 for _ in range(length)]
     for i in range(0, len(values), 2):
         row = [values[i]] + [char if char == 'O' else '' for char in values[i + 1]]
         table.append(row)
 
-        for j in range(length):
-            count[j] += 1 if values[i + 1][j] == 'O' else 0
-
-    return render_template('index.html', table=table, chart=count, label=list(range(1, length + 1)))
+    return render_template('index.html', table=table, label=list(range(1, length + 1)))
 
 
 @app.route("/callback", methods=['POST'])
@@ -68,7 +59,7 @@ def handle_text_message(event):
             line_bot_api.leave_room(room_id=event.source.room_id)
         return
 
-    if all(tag not in event.message.text for tag in ['#인증', '#ㅇㅈ']):
+    if not any(tag in event.message.text for tag in ['#인증', '#ㅇㅈ']):
         return
 
     if event.source.type == 'group':
@@ -83,10 +74,9 @@ def handle_text_message(event):
     else:
         return
 
-    if '#인증내역' in event.message.text:
-        url = f'{request.host_url}?gid={group_id}'
-        res = requests.get(f'http://tinyurl.com/api-create.php?url={url}')
-        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=res.text))
+    if any(tag in event.message.text for tag in ['#인증내역', '#인증현황']):
+        url = f'https://{request.host}/{group_id}'
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=url))
         return
 
     now = datetime.now()
