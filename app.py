28dff5955c77a20c9946435616b0e9109267c4e0
eunbeom.kang <eunbeom.kang@linecorp.com>
2020-07-03 00:15:58 +0900

    특정 스티커로 인증

--- app.py
+++ app.py
@@ -58,7 +58,7 @@ def callback():
 @handler.add(MessageEvent, message=StickerMessage)
 def handle_sticker_message(event):
     print(f'package_id : {event.message.package_id}, sticker_id : {event.message.sticker_id}')
-    if event.message.package_id == 1813268 and event.message.sticker_id == 25483443:
+    if event.message.package_id == '1813268' and event.message.sticker_id == '25483443':
         group_id, profile = get_profile(event)
         check(event, group_id, profile, '#ffff00')
 
